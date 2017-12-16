bb6e66fa184c012b65e8ec0bdba1637b23b71139
chike <chike@sfc.wide.ad.jp>
2017-12-16 14:23:03 +0900

    select number of order, next is submit order

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,15 +7,19 @@
 
     <title>Laravel</title>
 
-    <!-- Fonts -->
+    <!-- Fonts 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+    -->
+    <!-- Jquery -->
+    <script src="{{asset('/assets/js/jquery-3.2.1.min.js')}}"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 
     <!-- Styles -->
     <link href="{{asset('/assets/css/bootstrap.min.css')}}" rel="stylesheet">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <script src="{{asset('/assets/js/bootstrap.min.js')}}"></script>
 
-    <script src="{{asset('/assets/js/jquery-3.2.1.min.js')}}"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <style>
         body {
@@ -85,9 +89,5 @@
 
     @yield('content')
 
-    <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,14 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container text-center">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Order</div>
 
                 <div class="panel-body">
-                  This is order page
+                  <ul class="list-group">
+                      <li class="list-group-item" id="0">
+                        <p>マー油豚骨醤油</p>
+                        <span class="badge">0</span>
+                        <button class="btn btn-primary" data-toggle="modal" data-target="#orderModal" onclick="selectorder(0)">選択</button>
+                      </li>
+                      <li class="list-group-item" id="1">
+                        <p>味噌豚骨醤油</p>
+                        <span class="badge">0</span>
+                        <button class="btn btn-primary" data-toggle="modal" data-target="#orderModal" onclick="selectorder(1)">選択</button>
+                      </li>
+                      <li class="list-group-item" id="2">
+                        <p>豚骨醤油</p>
+                        <span class="badge">0</span>
+                        <button class="btn btn-primary" data-toggle="modal" data-target="#orderModal" onclick="selectorder(2)">選択</button>
+                      </li>
+                  </ul>
+                  <script>
+                    function selectorder(id){
+                        var num = $("#"+ id + " span")[0].innerHTML;
+                        var food = $("#"+ id + " p")[0].innerHTML;
+                        $("#foodname")[0].innerHTML = food;
+                        $("#foodid")[0].innerHTML = id;
+                    }
+                    function setordernum(){
+                        var ordernum = $("#orderModal #num").val();
+                        var id = $('#orderModal #foodid')[0].innerHTML;
+                        $("#" + id + " span")[0].innerHTML = ordernum;
+                    }
+                  </script>
+                    <!-- モーダル・ダイアログ -->
+                  <div class="modal fade" id="orderModal" tabindex="-1">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal"><span>×</span></button>
+                          <h4 class="modal-title">数量選択</h4>
+                        </div>
+                        <div class="modal-body">
+                          <div id="foodname"></div>
+                          <div id="foodid" style="display:none;"></div>
+                          <div class="form-group">
+                            <select class="form-control" id="num" name="数量">
+                              <option value="0">0</option>
+                              <option value="1">1</option>
+                              <option value="2">2</option>
+                              <option value="3">3</option>
+                            </select>
+                          </div>
+                          <button type="button" class="btn-primary" data-dismiss="modal" onclick="setordernum()">確定</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <button class="btn btn-primary" data-toggle="modal" data-target="#submitModal" onclick="ordersubmit()">注文</button>
+                  <!-- モーダル・ダイアログ -->
+                  <div class="modal fade" id="submitModal" tabindex="-1">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal"><span>×</span></button>
+                          <h4 class="modal-title">注文確認</h4>
+                        </div>
+                        <div class="modal-body">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
             </div>
         </div>
