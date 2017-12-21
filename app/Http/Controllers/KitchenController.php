40b0124b5827bd17c78e9edafe412be4a54724b5
chike <chike@sfc.wide.ad.jp>
2017-12-21 15:23:23 +0900

    kitchen

--- app/Http/Controllers/KitchenController.php
+++ app/Http/Controllers/KitchenController.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Orders;
+use App\Foods;
 
-class KitchenController extends Controller
-{
-  function index(){
-    return view("ketchen");
-  }
+class KitchenController extends Controller {
+    function index(){
+        $datas = Orders::all()->toArray();
+        $orders = [];
+        foreach($datas as $data){
+            $data["foodname"] = Foods::where(['id'=> $data["foodid"]])->get()->pluck("name")->first();
+            array_push($orders, $data);
+        }
+        return view("ketchen")->with("data", $orders);
+    }
 }
--- resources/views/ketchen.blade.php
+++ resources/views/ketchen.blade.php
@@ -5,10 +5,31 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">ketchen</div>
+                <div class="panel-heading">kitchen</div>
 
                 <div class="panel-body">
-                  This is ketchen page
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Food</th>
+                                <th>num</th>
+                                <th>share</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($data as $order)
+                            <tr>
+                                <td>{{$order["id"]}}</td>
+                                <td>{{ $order["foodname"] }}</td>
+                                <td>{{ $order["num"] }}</td>
+                                <td>0 / {{ $order["num"] }}</td>
+                                <td> <button class="btn-primary">完了</button> </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
